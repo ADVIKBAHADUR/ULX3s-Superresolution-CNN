e536a4bf74f280ae58413e3b25609dc1aa88c7d6
Advik Bahadur <advikbahadur@gmail.com>
2024-08-06 10:06:25 +0100

    added buffers

--- Sobel-SuperResolution Wrapper/src/sobel_convolution.v
+++ Sobel-SuperResolution Wrapper/src/sobel_convolution.v
@@ -8,10 +8,11 @@ module SuperResolutionSubTop(
     output wire [16:0] dout,
     output wire [9:0] data_count_r
 );
-    localparam init = 0, loop = 1;
-    
-    reg state_q, state_d;
-    reg [10:0] pixel_counter_q = 1920;
+    localparam FRAME_SIZE = 76800; // 320x240 pixels
+
+    reg [1:0] state_q, state_d;
+    reg [16:0] pixel_q, pixel_d;
+    reg [9:0] pixel_counter_q = 320;
     reg [4:0] r_channel;
     reg [5:0] g_channel;
     reg [4:0] b_channel;
@@ -21,22 +22,31 @@ module SuperResolutionSubTop(
 
     // Frame sync
     reg frame_sync;
-    reg [9:0] line_counter;
+    reg [7:0] line_counter;
 
     // Signals for dual_port_sync modules
     reg we_1, we_2, we_3, we_4, we_5, we_6;
     reg signed [7:0] din_ram_x, din_ram_y;
-    reg [9:0] addr_a_x, addr_a_y, addr_b_q, addr_b_d;
+    reg [8:0] addr_a_x, addr_a_y, addr_b_q, addr_b_d;
     wire signed [7:0] dout_1, dout_2, dout_3, dout_4, dout_5, dout_6;
+
+    // Double buffering
+    reg current_buffer;
+    reg [16:0] frame_buffer_1[0:FRAME_SIZE-1];
+    reg [16:0] frame_buffer_2[0:FRAME_SIZE-1];
+    reg frame_complete;
 
     always @(posedge clk_w or negedge rst_n) begin
         if (!rst_n) begin
-            state_q <= init;
-            pixel_counter_q <= 1920;
+            state_q <= 0;
+            pixel_counter_q <= 320;
             rd_en <= 1'b0;
             rd_fifo_cam <= 1'b0;
             frame_sync <= 1'b0;
             line_counter <= 0;
+            current_buffer <= 0;
+            frame_complete <= 0;
+            pixel_q <= 0;
         end else begin
             state_q <= state_d;
             rd_en <= 1'b0;
@@ -48,17 +58,25 @@ module SuperResolutionSubTop(
                 rd_en <= 1'b1;
                 rd_fifo_cam <= 1'b1;
                 
-                if (pixel_counter_q == 1919) begin
+                if (pixel_counter_q == 319) begin
                     pixel_counter_q <= 0;
-                    if (line_counter == 479) begin
+                    if (line_counter == 239) begin
                         line_counter <= 0;
                         frame_sync <= ~frame_sync;
+                        current_buffer <= ~current_buffer;
+                        frame_complete <= 1;
                     end else begin
                         line_counter <= line_counter + 1;
                     end
                 end else begin
                     pixel_counter_q <= pixel_counter_q + 1'b1;
                 end
+
+                // Store in the current buffer
+                if (current_buffer == 0)
+                    frame_buffer_1[line_counter * 320 + pixel_counter_q] <= din;
+                else
+                    frame_buffer_2[line_counter * 320 + pixel_counter_q] <= din;
             end
         end
     end
@@ -69,10 +87,10 @@ module SuperResolutionSubTop(
         din_ram_x = 0; din_ram_y = 0;
         addr_a_x = 0; addr_a_y = 0;
         
-        if (pixel_counter_q != 1920) begin
+        if (pixel_counter_q != 320) begin
             we_1 = 1; we_4 = 1;
-            addr_a_y = pixel_counter_q % 640;
-            addr_a_x = pixel_counter_q % 640;
+            addr_a_y = pixel_counter_q;
+            addr_a_x = pixel_counter_q;
             din_ram_y = {3'b0, r_channel};
             din_ram_x = {2'b0, g_channel};
         end
@@ -85,47 +103,47 @@ module SuperResolutionSubTop(
         state_d = state_q;
 
         case (state_q)
-            init: if (pixel_counter_q == 0 && data_available) begin
+            2'b00: if (frame_complete) begin
                 addr_b_d = 0;
-                state_d = loop;            
+                state_d = 2'b01;            
             end
-            loop: if (data_available) begin
-                addr_b_d = pixel_counter_q % 640;
+            2'b01: if (rd_fifo) begin
+                addr_b_d = (addr_b_q == FRAME_SIZE - 1) ? 0 : addr_b_q + 1;
                 write = 1;
-                // Combine all color channels
-                data_write = {frame_sync, r_channel, g_channel, b_channel};
+                // Read from the non-current buffer
+                data_write = current_buffer ? frame_buffer_1[addr_b_q] : frame_buffer_2[addr_b_q];
             end
-            default: state_d = init;
+            default: state_d = 2'b00;
         endcase 
     end
 
     // Dual port sync modules
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m0 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m0 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_1),
         .din(din_ram_y), .addr_a(addr_a_y), .addr_b(addr_b_d), .dout(dout_1)
     );
     
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m1 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m1 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_2),
         .din(din_ram_y), .addr_a(addr_a_y), .addr_b(addr_b_d), .dout(dout_2)
     );
     
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m2 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m2 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_3),
         .din(din_ram_y), .addr_a(addr_a_y), .addr_b(addr_b_d), .dout(dout_3)
     );
     
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m3 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m3 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_4),
         .din(din_ram_x), .addr_a(addr_a_x), .addr_b(addr_b_d), .dout(dout_4)
     );
     
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m4 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m4 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_5),
         .din(din_ram_x), .addr_a(addr_a_x), .addr_b(addr_b_d), .dout(dout_5)
     );
     
-    dual_port_sync #(.ADDR_WIDTH(10), .DATA_WIDTH(8)) m5 (
+    dual_port_sync #(.ADDR_WIDTH(9), .DATA_WIDTH(8)) m5 (
         .clk_r(clk_w), .clk_w(clk_w), .we(we_6),
         .din(din_ram_x), .addr_a(addr_a_x), .addr_b(addr_b_d), .dout(dout_6)
     );
